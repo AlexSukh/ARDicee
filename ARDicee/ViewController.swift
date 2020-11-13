2a70cfdd51eac1964560f7bc3157f5d100af7a83
Alex Sukhitashvili <sukhitashvilialex2@gmail.com>
2020-11-13 21:37:33 +0400

    feat: roll a dice

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -23,6 +23,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         sceneView.autoenablesDefaultLighting = true
 
+      
+        
+        
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -44,7 +48,41 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if let touch = touches.first {
+            
+            let touchLocation = touch.location(in: sceneView)
+            
+            let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+            
+            if let hitResult = results.first {
 
+                // Create a new scene
+                let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+
+                if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+
+                    diceNode.position = SCNVector3(
+                        x: hitResult.worldTransform.columns.3.x,
+                        y: hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
+                        z: hitResult.worldTransform.columns.3.z
+                    )
+
+                    sceneView.scene.rootNode.addChildNode(diceNode)
+                    
+                    let randomX = Float((arc4random_uniform(4) + 1)) * (Float.pi/2)
+                    //        let randomY = Double((arc4random_uniform(10) + 11)) * (Double.pi/2)
+                    let randomZ = Float((arc4random_uniform(4) + 1)) * (Float.pi/2)
+                    
+                    diceNode.runAction(SCNAction.rotateBy(x: CGFloat(randomX * 5), y: 0, z: CGFloat(randomZ * 5), duration: 0.5))
+
+                }
+                
+            }
+            
+        }
+    }
     
     // MARK: - ARSCNViewDelegate
     
